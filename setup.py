2fe2f7c4ac5023b24e64dfcc65a2c8d24ee07140
Jonathan MacCarthy <jkmacc@lanl.gov>
2015-09-14 16:49:02 -0600

    Add actual code.

---
+++ setup.py
@@ -0,0 +1,24 @@
+try:
+    import setuptool
+except:
+    pass
+
+import os
+import os.path
+import sys
+import commands
+from glob import glob
+from distutils.core import setup, Extension
+
+setup(name = 'pysac',
+      version = '0.0.1',
+      description = 'Python interface to the Seismic Analsys Code file format.',
+      author = 'J. MacCarthy',
+      author_email = 'jkmacc@lanl.gov',
+      long_description = '''
+Python interface to the Seismic Analsys Code file format.
+''',
+       packages = ['pysac'],
+       py_modules = ['pysac.header', 'pysac.util', 'pysac.arrayio',
+                     'pysac.sactrace'],
+)
